faf35a8c918e2959b0e52fb632abbb6e7013917c
Stephan Jahn <ste-j@web.de>
2015-04-18 13:25:10 +0200

    [game.py] implement basic subroutine to play with two human players

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 
+try:
+    import Tkinter as tk # python 2
+except ImportError:
+    import tkinter as tk # python 3
 import lib
 
 def play_game(white_player, black_player, game_board):
@@ -8,7 +12,7 @@ def play_game(white_player, black_player, game_board):
 
     :param white_player, black_player:
 
-        The players that shall play the game as defined in ``lib.players``
+        The players that shall play the game as defined in ``lib.player``
 
     :param game_board:
 
@@ -27,4 +31,13 @@ def play_game(white_player, black_player, game_board):
             return
 
 if __name__ == '__main__':
+    board = lib.board.Board(9,16)
+    window = tk.Tk()
+    gui = lib.gui.BoardGui(board, window)
+    player1 = lib.player.Human(lib.board.white, gui)
+    player2 = lib.player.Human(lib.board.black, gui)
+    play_game(player1, player2, board)
+    gui.renew_board()
+    print('End of game')
+    window.mainloop()
     raise NotImplementedError('The game is yet to be implemented')
