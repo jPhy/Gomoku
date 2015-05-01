f72cc2639da8f5d4a259e71fcec2514afb824f3e
Stephan Jahn <ste-j@web.de>
2015-05-01 18:35:10 +0200

    [game.py] only catch real Errors (not ``BaseException``)

--- game.py
+++ game.py
@@ -8,12 +8,15 @@ class ErrorHandler(object):
         pass
 
     def __exit__(self, error_type, error_args, traceback):
+        if error_type is None:
+            return
         if error_type is lib.gui.tk.TclError:
             print("WARNING: A GUI-related `TclError` error occurred:", error_args)
             return True # do not reraise error
         # else:
         # GUIify errors
-        lib.gui.Message(message=repr(error_args), icon='warning', title='Gomoku - error').show()
+        if issubclass(error_type, Exception): # Only catch real exceptions not ``BaseException``
+            lib.gui.Message(message=repr(error_args), icon='warning', title='Gomoku - error').show()
         return False # reraise nonTcl errors => stderr message and nonzero exit
 
 if __name__ == '__main__':
