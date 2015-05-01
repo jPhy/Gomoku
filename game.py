d7759a1a7c2a20437ae1de6bf5b66b5c12715a06
Stephan Jahn <ste-j@web.de>
2015-05-01 17:49:38 +0200

    [game.py] show all nonTcl errors as GUI message before exiting

--- game.py
+++ game.py
@@ -11,7 +11,10 @@ class ErrorHandler(object):
         if error_type is lib.gui.tk.TclError:
             print("WARNING: A GUI-related `TclError` error occurred:", error_args)
             return True # do not reraise error
-        return False # reraise nonTcl errors
+        # else:
+        # GUIify errors
+        lib.gui.Message(message=repr(error_args), icon='warning', title='Gomoku - error').show()
+        return False # reraise nonTcl errors => stderr message and nonzero exit
 
 if __name__ == '__main__':
     main_window = lib.gui.MainWindow(13,16)
