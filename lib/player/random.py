208a4f63d2b0ed3f41b29742c43ff9c98da4d45b
Stephan Jahn <ste-j@web.de>
2015-05-02 22:42:09 +0200

    [player.random] run ``win_if_possible`` before placing randomly

--- lib/player/random.py
+++ lib/player/random.py
@@ -5,6 +5,8 @@ class Random(Player):
     "An AI that randomly places stones."
     name = 'Random AI'
     def _make_move(self, gui):
+        if self.win_if_possible(gui):
+            return
         moves_left = gui.board.moves_left
         if moves_left > 0:
             while moves_left == gui.board.moves_left:
