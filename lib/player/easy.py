eec3d0e9318ad2fd8b70ba1e1eb011c38ff3fa8c
Stephan Jahn <ste-j@web.de>
2015-05-10 18:25:58 +0200

    [player/easy] improve AI

--- lib/player/easy.py
+++ lib/player/easy.py
@@ -2,10 +2,14 @@ from . import Player, InvalidMoveError
 import numpy as np
 
 class Easy(Player):
-    "An AI that randomly places stones."
     name = 'Easy'
     def _make_move(self, gui):
-        if self.win_if_possible(gui):
-            return
-        else:
+        if self.win_if_possible(gui): return
+        if self.extend_three_to_four(gui): return
+        if self.block_open_three(gui): return
+        if self.block_doubly_open_two(gui): return
+        if self.extend_two_to_three(gui): return
+        try:
+            gui.board[gui.board.height // 2, gui.board.width // 2] = self.color
+        except InvalidMoveError:
             self.random_move(gui)
